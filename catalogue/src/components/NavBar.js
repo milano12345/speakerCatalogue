08583bcbb1c2bcd4ffbb8305c33bf109ab24fd99
milano12345 <dmilano857@gmail.com>
2020-10-01 00:31:52 -0600

    updated NavBar component to be responsive

--- catalogue/src/components/NavBar.js
+++ catalogue/src/components/NavBar.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const NavBar = () => (
-  <nav>
-    <div>This is the nav bar</div>
-  </nav>
-);
+const NavBar = () => {
+  useEffect(() => {
+    //refCount.current += 1;
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  });
+  return (
+    <div className="topnav" id="myTopnav">
+      <a href="#home" className="active">
+        Home
+      </a>
+      <a href="#news">About</a>
+      <div className="dropdown">
+        <button className="dropbtn">
+          Catalogue
+          <i className="fa fa-caret-down"></i>
+        </button>
+        <div className="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+      <a href="#contact">Contact</a>
+    </div>
+  );
+};
 
 export default NavBar;
