5364b66445b973c58d3e4768b257e7a8a3038557
milano12345 <dmilano857@gmail.com>
2020-11-05 21:40:57 -0700

    updated card styles with shadows

--- catalogue/src/components/ClassCards.js
+++ catalogue/src/components/ClassCards.js
@@ -8,10 +8,10 @@ const ClassCards = (props) => {
       <div className={"price"}>{props.data.price}</div>
       <img className={"productImage"} src={props.data.productImg}></img>
       <div className={"shippingInfo"}>{props.data.shipping}</div>
-      <div className={"productSpec"}>{props.data.specs}</div>
+      {/* <div className={"productSpec"}>{props.data.specs}</div>
       <div className={"productReviews"}>{props.data.reviews}</div>
       <div className={"similarProducts"}>{props.data.similiar}</div>
-      <div className={"recentlyViewed"}>{props.data.recentlyViewed}</div>
+      <div className={"recentlyViewed"}>{props.data.recentlyViewed}</div> */}
     </div>
   );
 };
